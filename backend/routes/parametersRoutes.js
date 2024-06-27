3e3c090b5e79c95a7ca36e5c506ee37bcba8907e
xavier290 <xa25alex@gmail.com>
2024-06-27 12:57:45 -0600

    fixing route not being uploaded issue

--- backend/routes/parametersRoutes.js
+++ backend/routes/parametersRoutes.js
@@ -1,9 +1,10 @@
 // backend/routes/parametersRoutes.js
 const express = require('express');
 const router = express.Router();
-const { updateParameters } = require('../controllers/parametersController');
+const { updateParameters, getParameters } = require('../controllers/parametersController');
 
 // Update Parameters
 router.put('/:id', updateParameters);
+router.get('/info/:id', getParameters);
 
 module.exports = router;
